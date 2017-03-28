372762d4f65186cce1a23c51f7c4e57574b4f7a6
otecfura <fura.otec@gmail.com>
2017-03-28 14:11:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# SlackFood
+Simple python3 script for parse restaurant daily menu from Zomato and post it to the slack via webhook.
+
+# Config
+Fill the config.sample.json with your credentials and rename it to the sample.json
+
+Fill the Zomato ids and names of your restaurants to restaurants.json
+
+# Requirements
+only requests library, you can install it by this code:
+
+> pip3 install -r requirements.txt
+
+# Running
+If you want to debug, you can run:
+> python3 main.py
+
+and output will be printed to console.
+
+
+If you want post it via webhook you can run:
+> python3 main.py production
+
+and output will be posted via webhook.
---
+++ main.py
@@ -0,0 +1,75 @@
+import json
+import requests
+import pprint
+import sys
+
+
+class Slack:
+
+    def __init__(self):
+        self.hook_link = get_data_from_token("slack-web-hook-link")
+
+    def post(self, menus):
+        for menu in menus:
+            post_data = {"text": menu}
+            if is_debug():
+                pp = pprint.PrettyPrinter(indent=4)
+                pp.pprint(json.dumps(post_data))
+            else:
+                data = json.dumps(post_data, ensure_ascii=False).encode('utf8')
+                requests.post(self.hook_link, data=data)
+
+
+class Zomato:
+
+    def __init__(self):
+        self.token = get_data_from_token("zomato-token")
+
+    def get_daily_dishes(self, res_id, name):
+        payload = {'res_id': res_id}
+        headers = {'user_key': self.token}
+        r = requests.get(
+            "https://developers.zomato.com/api/v2.1/dailymenu",
+            params=payload,
+            headers=headers)
+        dishes = r.json()["daily_menus"][0]["daily_menu"]["dishes"]
+        w_price = list(filter(lambda x: len(x["dish"]["price"]) > 0, dishes))
+        return self.format_data(w_price, name)
+
+    def format_data(self, data, restaurant_name):
+        dishes = "*" + restaurant_name + "*" + "\n"
+        for dish in data:
+            dishes += dish["dish"]["name"] + "\n"
+        return dishes
+
+    def read_restaurants(self):
+        with open('restaurants.json', encoding="utf-8") as restaurants_file:
+            restaurants = json.load(restaurants_file)
+        return restaurants
+
+
+def get_data_from_token(id):
+    with open('config.json', encoding="utf-8") as tokens_file:
+        tokens_json = json.load(tokens_file)
+        token = tokens_json[id]
+    return token
+
+
+def is_debug():
+    DEBUG = True
+    if len(sys.argv) > 1:
+        DEBUG = sys.argv[1] != "production"
+    return DEBUG
+
+
+zom = Zomato()
+
+menus = list()
+
+for restaurant in zom.read_restaurants():
+    rest_id = restaurant["id"]
+    rest_name = restaurant["name"]
+    menus.append(zom.get_daily_dishes(rest_id, rest_name))
+
+slack = Slack()
+slack.post(menus)
